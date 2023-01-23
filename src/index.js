a40df7482a75ada48527138bd1f4b3b8cc7d7a62
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-01-23 19:54:35 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,121 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './css/styles.css';
+import '../node_modules/w3-css/'
+import { fetchPhoto } from './services/fetchPhoto';
+
+
+// https://pixabay.com/api/?key=24319786-e6f55023f5bc4aeea2cc437aa&q=yellow+flowers&image_type=photo
+
+const button = document.querySelector('button');
+const search = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const perPage = 40;
+let page = 1;
+let searchQuery;
+loadMoreBtn.classList.add('is-hidden');
+
+
+const handleSubmit = event => {
+    event.preventDefault();
+    gallery.innerHTML = '';
+    page = 1;
+    searchQuery = event.currentTarget.searchQuery.value.trim();
+    fetchPhoto(searchQuery, page, perPage).then(photos => {
+        if (photos.totalHits < perPage) {
+            Notiflix.Notify.success(`We found ${photos.totalHits} results`, { timeout: 1000 });
+            drawPhotos(photos);
+            setTimeout(warning, 1500)
+        } else {
+            proceed(photos);
+        }
+    }).catch(error);
+}
+
+// const handleLoadMore = event => {
+//   event.preventDefault();
+//   fetchPhoto(searchQuery, page, perPage).then(photos => {
+//     let totalPages = photos.totalHits / perPage;
+//     if (page >= totalPages) {
+//       warning();
+//   } drawPhotos(photos)
+//   });
+//   page += 1;
+// }
+
+function onEntry(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            fetchPhoto(searchQuery, page, perPage).then(photos => {
+                let totalPages = photos.totalHits / perPage;
+                if (page >= totalPages) {
+                    warning();
+                } drawPhotos(photos)
+            });
+            page += 1;
+        }
+    });
+
+}
+
+
+search.addEventListener('submit', handleSubmit);
+// loadMoreBtn.addEventListener('click', handleLoadMore);
+
+const observer = new IntersectionObserver(onEntry, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+});
+observer.observe(loadMoreBtn);
+
+function proceed(photos) {
+    drawPhotos(photos);
+    page += 1;
+    loadMoreBtn.classList.remove('is-hidden');
+    Notiflix.Notify.success(`We found ${photos.totalHits} results`, { timeout: 1000 });
+}
+
+function warning() {
+    loadMoreBtn.classList.add('is-hidden');
+    Notiflix.Notify.warning("Were sorry, but you've reached the end of search results.", { timeout: 1000 });
+}
+
+function drawPhotos(photos) {
+    const markup = photos.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+    <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    </a>
+  <div class="info">
+    <p class="info-item">
+      <b><i class="fa fa-heart-o" aria-hidden="true">&nbsp;</i>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b><i class="fa fa-eye" aria-hidden="true">&nbsp;</i>${views}</b>
+    </p>
+    <p class="info-item">
+      <b><i class="fa fa-comments-o" aria-hidden="true">&nbsp;</i>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b><i class="fa fa-download" aria-hidden="true">&nbsp;</i>${downloads}</b>
+    </p>
+  </div>
+</div>`).join('');
+    gallery.insertAdjacentHTML('beforeend', markup);
+    lghtbx();
+}
+
+
+function error(error) {
+    loadMoreBtn.classList.add('is-hidden');
+}
+
+function lghtbx() {
+    const lightbox = new SimpleLightbox('.photo-card a');
+    lightbox.on('show.simplelightbox');
+    lightbox.refresh()
+}
---
+++ src/services/fetchPhoto.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+const loadMoreBtn = document.querySelector('.load-more')
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export async function fetchPhoto(searchQuery, page, perPage) {
+    // const {} = await axios.get(``);
+    try {
+        const { data } = await axios.get(`?key=24319786-e6f55023f5bc4aeea2cc437aa&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+        console.log(data);
+        if (data.hits.length === 0 || searchQuery === '') {
+            return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.', { timeout: 1000 });
+        } else {
+            return data;
+        }
+    } catch (error) {
+        error => console.log(error);
+    }
+    // .then(response => {
+    //     if (!response.ok) {
+    //         throw new Error(response.status);
+    //     }
+    //    return response.json();
+    // });
+};
