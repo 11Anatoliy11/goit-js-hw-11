e3da534c0a2c5163094034546dadc2b30b45348f
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-01-26 09:37:31 +0200

    Готовий

--- src/index.js
+++ src/index.js
@@ -1,121 +1,92 @@
-import axios from 'axios';
+import './sass/main.scss';
+import { fetchImages } from './js/fetch-images';
+import { renderGallery } from './js/render-gallery';
+import { onScroll, onToTopBtn } from './js/scroll';
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import './css/styles.css';
-import '../node_modules/w3-css/'
-import { fetchPhoto } from './services/fetchPhoto';
 
-
-// https://pixabay.com/api/?key=24319786-e6f55023f5bc4aeea2cc437aa&q=yellow+flowers&image_type=photo
-
-const button = document.querySelector('button');
-const search = document.querySelector('#search-form');
+const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const perPage = 40;
+const loadMoreBtn = document.querySelector('.btn-load-more');
+let query = '';
 let page = 1;
-let searchQuery;
-loadMoreBtn.classList.add('is-hidden');
+let simpleLightBox;
+const perPage = 40;
 
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
-const handleSubmit = event => {
-    event.preventDefault();
-    gallery.innerHTML = '';
+onScroll();
+onToTopBtn();
+
+function onSearchForm(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0 });
     page = 1;
-    searchQuery = event.currentTarget.searchQuery.value.trim();
-    fetchPhoto(searchQuery, page, perPage).then(photos => {
-        if (photos.totalHits < perPage) {
-            Notiflix.Notify.success(`We found ${photos.totalHits} results`, { timeout: 1000 });
-            drawPhotos(photos);
-            setTimeout(warning, 1500)
-        } else {
-            proceed(photos);
-        }
-    }).catch(error);
+    query = e.currentTarget.searchQuery.value.trim();
+    gallery.innerHTML = '';
+    loadMoreBtn.classList.add('is-hidden');
+
+    if (query === '') {
+        alertNoEmptySearch();
+        return;
+    }
+
+    fetchImages(query, page, perPage)
+        .then(({ data }) => {
+            if (data.totalHits === 0) {
+                alertNoImagesFound();
+            } else {
+                renderGallery(data.hits);
+                simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+                alertImagesFound(data);
+
+                if (data.totalHits > perPage) {
+                    loadMoreBtn.classList.remove('is-hidden');
+                }
+            }
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+            searchForm.reset();
+        });
 }
 
-// const handleLoadMore = event => {
-//   event.preventDefault();
-//   fetchPhoto(searchQuery, page, perPage).then(photos => {
-//     let totalPages = photos.totalHits / perPage;
-//     if (page >= totalPages) {
-//       warning();
-//   } drawPhotos(photos)
-//   });
-//   page += 1;
-// }
-
-function onEntry(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            fetchPhoto(searchQuery, page, perPage).then(photos => {
-                let totalPages = photos.totalHits / perPage;
-                if (page >= totalPages) {
-                    warning();
-                } drawPhotos(photos)
-            });
-            page += 1;
-        }
-    });
-
-}
-
-
-search.addEventListener('submit', handleSubmit);
-// loadMoreBtn.addEventListener('click', handleLoadMore);
-
-const observer = new IntersectionObserver(onEntry, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5,
-});
-observer.observe(loadMoreBtn);
-
-function proceed(photos) {
-    drawPhotos(photos);
+function onLoadMoreBtn() {
     page += 1;
-    loadMoreBtn.classList.remove('is-hidden');
-    Notiflix.Notify.success(`We found ${photos.totalHits} results`, { timeout: 1000 });
+    simpleLightBox.destroy();
+
+    fetchImages(query, page, perPage)
+        .then(({ data }) => {
+            renderGallery(data.hits);
+            simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+            const totalPages = Math.ceil(data.totalHits / perPage);
+
+            if (page > totalPages) {
+                loadMoreBtn.classList.add('is-hidden');
+                alertEndOfSearch();
+            }
+        })
+        .catch(error => console.log(error));
 }
 
-function warning() {
-    loadMoreBtn.classList.add('is-hidden');
-    Notiflix.Notify.warning("Were sorry, but you've reached the end of search results.", { timeout: 1000 });
+function alertImagesFound(data) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
 }
 
-function drawPhotos(photos) {
-    const markup = photos.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-        `<div class="photo-card">
-    <a href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-  <div class="info">
-    <p class="info-item">
-      <b><i class="fa fa-heart-o" aria-hidden="true">&nbsp;</i>${likes}</b>
-    </p>
-    <p class="info-item">
-      <b><i class="fa fa-eye" aria-hidden="true">&nbsp;</i>${views}</b>
-    </p>
-    <p class="info-item">
-      <b><i class="fa fa-comments-o" aria-hidden="true">&nbsp;</i>${comments}</b>
-    </p>
-    <p class="info-item">
-      <b><i class="fa fa-download" aria-hidden="true">&nbsp;</i>${downloads}</b>
-    </p>
-  </div>
-</div>`).join('');
-    gallery.insertAdjacentHTML('beforeend', markup);
-    lghtbx();
+function alertNoEmptySearch() {
+    Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
 }
 
-
-function error(error) {
-    loadMoreBtn.classList.add('is-hidden');
+function alertNoImagesFound() {
+    Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+    );
 }
 
-function lghtbx() {
-    const lightbox = new SimpleLightbox('.photo-card a');
-    lightbox.on('show.simplelightbox');
-    lightbox.refresh()
+function alertEndOfSearch() {
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
 }
